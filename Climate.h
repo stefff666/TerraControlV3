2154d2b5e1ea25792f4b5587716bc492ba62b952
stefff666 <162278109+stefff666@users.noreply.github.com>
2026-07-23 03:14:23 +0930

    Update Climate.h

--- Climate.h
+++ Climate.h
@@ -1,1 +1,51 @@
+#pragma once
 
+/***********************************************************************
+ * TerraControl V3
+ * Climate Manager
+ ***********************************************************************/
+
+#include <Arduino.h>
+#include <DHT.h>
+
+#include "Config.h"
+
+class ClimateManager
+{
+public:
+
+    ClimateManager();
+
+    // Initialise hardware
+    void begin();
+
+    // Called every loop()
+    void update();
+
+    // Returns true if the DHT is reading correctly
+    bool sensorOK() const;
+
+private:
+
+    DHT dht;
+
+    unsigned long lastRead = 0;
+
+    void readSensors();
+
+    float readSoil();
+
+    void controlTemperature();
+
+    void controlHumidity();
+
+    void setHeater(bool state);
+
+    void setHumidifier(bool state);
+
+    void setFan(bool state);
+};
+
+// Global object
+
+extern ClimateManager Climate;
